72378f1cf985d4ce41dca7c9e0ebd4c2568cd5cf
김진영 <skswls0@daum.net>
2022-12-28 19:07:27 +0900

    Header, Footer 생성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,21 @@
+import styled from 'styled-components';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+
+const Container = styled.div`
+   max-width: 128rem;
+   margin: 0 auto;
+   padding: 0 1rem;
+`;
+
 const Main = () => {
-   return <div>Main</div>;
+   return (
+      <>
+         <Header />
+         <Container>Main</Container>
+         <Footer />
+      </>
+   );
 };
 
 export default Main;
